60fe837532c3533a7ea09a6b86376105e7e119eb
lunaria20 <katepotane@gmail.com>
2025-09-26 17:18:59 +0800

    feature(SmartTrav): complete app setup

--- smart_trav_plan/SmartTrav/models.py
+++ smart_trav_plan/SmartTrav/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Destination(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    location = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='media/destinations/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
---
+++ smart_trav_plan/static/SmartTrav/css/js/script.js
@@ -0,0 +1,1 @@
+console.log("SmartTrav JS loaded 🌴✨");
