65c230851d97ab2a6aa220ad26957f262a57bc83
lunaria20 <katepotane@gmail.com>
2025-09-26 17:35:46 +0800

    chore(media): update settings, urls, models and add media folder with .gitkeep

--- smart_trav_plan/SmartTrav/models.py
+++ smart_trav_plan/SmartTrav/models.py
@@ -4,8 +4,7 @@ class Destination(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     location = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='media/destinations/', blank=True, null=True)
+    image = models.ImageField(upload_to='destinations/', blank=True, null=True)  # ✅ fixed path
 
     def __str__(self):
         return self.name
-
--- smart_trav_plan/smart_trav_plan/settings.py
+++ smart_trav_plan/smart_trav_plan/settings.py
@@ -115,11 +115,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-# Static files
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
-# Media files
+# Media files (user uploads like images)
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
