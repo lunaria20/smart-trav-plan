59632e76503bba6c6f907953c7a91dd364074ccb
Keint Warren <keintwarren.poliquit@cit.edu>
2025-12-09 13:42:42 +0800

    refactor: clean up comments and code formatting

--- smart_trav_plan/SmartTrav/urls.py
+++ smart_trav_plan/SmartTrav/urls.py
@@ -47,3 +47,6 @@ urlpatterns = [
 # --- MEDIA FILE CONFIGURATION FOR DEVELOPMENT ONLY ---
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
